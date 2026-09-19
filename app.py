235750b0c8f488e7927607c74b3ab209e7aaf1e0
midhat-fatima03 <midhatfatima646@gmail.com>
2026-09-19 13:51:15 +0530

    Fix: update text_splitter import path for newer langchain version

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ import streamlit as st
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
--- ingest.py
+++ ingest.py
@@ -19,7 +19,7 @@ Run this ONCE whenever you add or change files inside /data:
 
 import os
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
